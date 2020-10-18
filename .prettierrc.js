ea40eea254be5a734cd056e9d91367455ae721d7
hadnet <hadnet1@gmail.com>
2020-10-18 19:38:48 -0300

    add eslint, prettier and TS

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports =  {
+    semi:  true,
+    trailingComma:  'all',
+    singleQuote:  true,
+    printWidth:  180,
+    tabWidth:  2,
+    bracketSpacing: false,
+    arrowParens: 'avoid'
+};
