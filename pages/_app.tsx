52a99764b3fdf52c9b19efbc31ba2c98ccd0e23d
hadnet <hadnet1@gmail.com>
2020-10-27 21:23:50 -0300

    build: add styled-components settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
+import React from 'react';
 import {AppProps} from 'next/app';
-import '../styles/globals.css';
+import {createGlobalStyle, ThemeProvider, DefaultTheme} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    color: #161616;
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#0070f3',
+    border: '#eaeaea',
+  },
+};
 
 function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
